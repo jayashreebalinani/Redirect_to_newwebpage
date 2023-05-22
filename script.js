874ae69757265144f01470b0259771c015d20520
Jayashree Balineni <115000642+jayashreebalinani@users.noreply.github.com>
2023-05-22 19:28:28 +0530

    Update and rename redirect.js to script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+var info = [
+   {
+        Username:"Jayashree",
+        Password:"jaya*514"
+    },
+    {
+        Username:"Ushasree",
+        Password:"usha*514"
+    },
+    {
+        Username:"Vishnu",
+        Password:"vishnu*514"
+    },
+    {
+        Username:"Naveen",
+        Password:"naveen*514"
+    },
+   {
+        Username:"Abhi",
+        Password:"abhi*514"
+    },
+
+]
+
+function display(){
+var name1 = document.getElementById("user").value
+var passcode = document.getElementById("code").value
+for(var i = 0; i < info.length; i++){
+    if(info[i].Username == name1 && info[i].Password == passcode)
+    {
+        //newpage()
+        window.open('https://jayashreebalinani.github.io/Redirect_to_New_Webpage/', '_blank');
+
+    }
+}
+}
